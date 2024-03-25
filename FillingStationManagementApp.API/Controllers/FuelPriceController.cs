8700b11a93b8a00ae09b5e9ee80d9f74e80941e5
Benjamin Olumuyiwa <benjaminsqlserver@gmail.com>
2024-03-25 15:01:21 +0100

    End Of Controller Implementation

---
+++ FillingStationManagementApp.API/Controllers/FuelPriceController.cs
@@ -0,0 +1,41 @@
+﻿using FillingStationManagementApp.Application.Commands;
+using FillingStationManagementApp.Application.Queries;
+using FillingStationManagementApp.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FillingStationManagementApp.API.Controllers
+{
+
+    public class FuelPriceController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public FuelPriceController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<FuelPriceResponse>>> GetFuelPriceListForFillingStation(string fillingStationName)
+        {
+            var query = new GetPriceListForFillingStationQuery(fillingStationName);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<FuelPriceResponse>> CreateFuelPrice([FromBody] CreateFuelPriceCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+
+
+
+    }
+}
---
+++ FillingStationManagementApp.API/Controllers/FuelTypeController.cs
@@ -0,0 +1,41 @@
+﻿using FillingStationManagementApp.Application.Commands;
+using FillingStationManagementApp.Application.Queries;
+using FillingStationManagementApp.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FillingStationManagementApp.API.Controllers
+{
+    
+    public class FuelTypeController  : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public FuelTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<FuelTypeResponse>>> GetFuelTypeByName(string fuelTypeName)
+        {
+            var query = new GetFuelTypeByNameQuery(fuelTypeName);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<FuelTypeResponse>> CreateFuelType([FromBody] CreateFuelTypeCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+       
+
+
+    }
+}
